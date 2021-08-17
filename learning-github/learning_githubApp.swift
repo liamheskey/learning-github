4b06a3d55299c52ae22fb5c1b401b3385d6a8f37
Liam Heskey <jashesk2012@gmail.com>
2021-08-17 17:29:38 +0100

    Initial Commit

---
+++ learning-github/learning_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_githubApp.swift
+//  learning-github
+//
+//  Created by Liam Heskey on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct learning_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
